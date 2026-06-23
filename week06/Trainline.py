28a23a88cdefde11e70a18d41cbdbbd7559ad6fc
Leo Irakliotis <lgreco@gmail.com>
2026-06-23 11:18:35 -0500

    O(1)

--- week06/Trainline.py
+++ week06/Trainline.py
@@ -5,8 +5,31 @@ class Trainline:
         # Even its head station is empty. That's the hallmark
         # of an empty train line: head == empty
         self._head = None
+        self._tail = None
 
-    def add(self, new_station):
+    def add(self, new_item):
+        # Check if line is empty
+        if self._head is None:
+            # Line is empty
+            self._head = new_item
+        else:
+            # The line is not empty
+            self._tail.set_next(new_item)
+        # The tail can now point to the new station just added
+        self._tail = new_item
+
+
+
+    def add_old_fashion(self, new_station):
+        """Adds a new station to the end of the train line.
+
+        Input:
+        ------
+        new_station : Station
+          A station object to be added at the end of the trainline.
+
+        This method is deprecated.
+        """
         if self._head is None:
             # The line is empty. Make the new station its head station
             self._head = station
