b9b0a8a31206676d60673fdb57fa1f2e93f6e1c4
Leo Irakliotis <lgreco@gmail.com>
2026-07-28 12:21:34 -0500

    3bcafe9c82069c45e5353211a1bb3283

--- week11/HotelAlphabetical.py
+++ week11/HotelAlphabetical.py
@@ -8,3 +8,9 @@ def assign_room(guest_last_name) -> bool:
 
     first_letter = guest_last_name.upper()[0]
     first_ascii = ord(first_letter)
+    # room_idx = first_ascii - ASCII_A
+    room_idx = first_ascii % LETTERS
+    if rooms[room_idx] is None:
+        rooms[room_idx] = guest_last_name
+    else:
+        print("sorry")
